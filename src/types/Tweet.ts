b551288963d76f87f3912fc918dad9a70c2dc2d7
Oumaima-ElGana1 <oumaimaelg57460@gmail.com>
2026-09-14 11:40:21 +0200

    Nom et premon

---
+++ src/types/Tweet.ts
@@ -0,0 +1,12 @@
+export type Tweet ={
+    id : string //UUID
+    authorName : string //nom de l'auteur
+    authorHand : string //c'est le nom de l'utilisateur sans le @
+    content : string //le contenu du tweet
+    image : TweetImage
+    createdAt : string //la date doit être au format ISO 8601
+}
+export type TweetImage ={
+    url : string
+    alt : string 
+}
